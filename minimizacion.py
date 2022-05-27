b72d3c9936a2c9713f46d1e435afd6f5c2986b4a
Alex Nava <enava2@ucol.mx>
2022-05-26 22:20:03 -0500

    Final

--- minimizacion.py
+++ minimizacion.py
@@ -5,9 +5,9 @@ def minimizarAFD(Automata):
     partition = []
     tempArr = []
     tmp = []
+    estados = []
     partition.append(Automata.fStates)
     diQ = dict(zip(Automata.states, Automata.deltaStates))
-    print(diQ)
     try:
         for q in Automata.states:
             if (q not in partition[0]):
@@ -28,7 +28,8 @@ def minimizarAFD(Automata):
     #   [[Q1],[Q2, Q3],[Q4]]
     
     partition = []
-    estados = Automata.states
+    estados = Automata.states.copy()
+    #backup = Automata.states
 
     while (len(estados) > 0):
         for i in estados:
@@ -36,10 +37,59 @@ def minimizarAFD(Automata):
             #A -> [] from [[], [], []]
             for u in estados:
                 if (diQ[i] == diQ[u] and i != u):
-                    tmp.append(u)
-                    estados.remove(u)
-            tmp.append(i)
-            estados.remove(i)
+                    if (u not in tmp):
+                        tmp.append(u)
+                if(i not in tmp):
+                    tmp.append(i)
             partition.append(tmp)
+            for E in tmp:
+                if (E in estados):
+                    estados.remove(E)
     
-    print(partition)
+    print(f"πF = {partition}")
+    print()
+    input("...PRESIONE ENTER PARA CONTINUAR...")
+    os.system("cls")
+
+    #remplazo
+    quitar = []
+    YES = False
+    for grupo in partition:
+        #Quitar desde states
+        if (len(grupo) > 1):
+            for estado in Automata.states:
+                if ((estado in grupo) and (estado != grupo[0])):
+                    quitar.append(estado)
+            for estado in quitar:
+                Automata.states.remove(estado)
+            quitar = []
+            for estado in Automata.fStates:
+                if ((estado in grupo) and (estado != grupo[0])):
+                    quitar.append(estado)
+            for estado in quitar:
+                Automata.fStates.remove(estado)
+            quitar = []
+            try:
+                Automata.deltaStates = []
+                for estado in Automata.states:
+                    Automata.deltaStates.append(diQ[estado])
+            except:
+                input("ERROR EN QUITAR DELTA")
+            for miniPart in Automata.deltaStates:
+                for estado in range(len(miniPart)):
+                    if ((miniPart[estado] in grupo) and (miniPart[estado] != grupo[0])):
+                        miniPart[estado] = grupo[0]
+    
+    diQ = dict(zip(Automata.states, Automata.deltaStates))
+    print("########- AUTOMATA FINITO DETERMINISTA SIMPLIFICADO -########")
+    print(len(Automata.states))
+    print(len(Automata.sigma))
+    print(len(Automata.fStates))
+    print("Sig{", Automata.sigma, "}")
+    print("F{")
+    print(Automata.fStates)
+    print("}")
+    print("{")
+    for estado in range(len(Automata.states)):
+        print(f"{Automata.states[estado]} > {Automata.deltaStates[estado]}")
+    print("}")
